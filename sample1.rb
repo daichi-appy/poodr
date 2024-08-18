6d9a75aa0cf4df2dfaa89b3a7556d1c49b40cc85
Daichi-appy <daichi.es47@gmail.com>
2024-08-19 02:45:54 +0900

    依存関係があるコード追加

---
+++ sample1.rb
@@ -0,0 +1,27 @@
+class Engine
+  # horsepowerは、エンジンの馬力。fuel_typeは燃料の種類。
+  def initialize(horsepower, fuel_type)
+    @horsepower = horsepower
+    @fuel_type = fuel_type
+  end
+
+  def start
+    "Engine is starting..."
+  end
+end
+
+class Car
+  def initialize(color, horsepower, fuel_type)
+    @color = color
+    @horsepower = horsepower
+    @fuel_type = fuel_type
+  end
+
+  def drive
+    Engine.new(horsepower, fuel_type).start
+  end
+end
+
+# 実行例
+car = Car.new("red", 100)
+puts car.drive
